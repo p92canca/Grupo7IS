556955b8edbf50b804e198fb2370f2c670136c7d
Federico Diaz Benitez <i72dibefm@uco.es>
2014-12-03 16:53:17 +0100

    Creado proyecto C++

---
+++ Agenda/src/Agenda.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Agenda.cpp
+// Author      : Grupo7
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
