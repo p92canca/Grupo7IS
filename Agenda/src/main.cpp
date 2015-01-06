3a9c40e91fd70560953e38bc31dd9d48d58b9dea
Federico Díaz <platonzaid@gmail.com>
2015-01-06 16:39:43 +0100

    Modificaciones en el main
    Pequeñas modificaciones en al main

--- Agenda/src/main.cpp
+++ Agenda/src/main.cpp
@@ -7,12 +7,94 @@
 //============================================================================
 
 #include <iostream>
+#include <stdlib.h>
 #include "Agenda.h"
 
 using namespace std;
 using namespace Dentista;
 
+int menu();
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	system("cls");
+
+	int opcion;
+
+	do{
+		opcion = menu();
+
+		switch(opcion)
+		{
+			case 0:
+			{
+				cout << endl << "\t\tHa seleccionado salir.";
+				getchar();
+				getchar();
+
+				break;
+			}
+
+			case 1:
+			{
+
+				break;
+			}
+
+			case 2:
+			{
+
+				break;
+			}
+
+			case 3:
+			{
+
+				break;
+			}
+
+			case 4:
+			{
+
+				break;
+			}
+
+			case 5:
+			{
+
+				break;
+			}
+
+			default:
+			{
+				cout << endl << "\t\tOpción incorrecta. Inténtelo de nuevo.";
+				getchar();
+				getchar();
+			}
+		}
+	}while(opcion != 0);
+
 	return 0;
+}
+
+int menu() {
+	int opcion;
+
+	do{
+		system("cls");
+
+		cout << "MENU DEL SISTEMA AGENDA" << endl;
+		cout << "***********************" << endl;
+		cout << "(1) Buscar datos de paciente" << endl;
+		cout << "(2) Mostrar consultas frecuentes" << endl;
+		cout << "(3) Mostrar favoritos" << endl;
+		cout << "(4) Modificar datos de paciente" << endl;
+		cout << "(5) Borrar datos de paciente" << endl;
+		cout << "(6) Generar informe" << endl;
+		cout << "(0) Salir" << endl << endl;
+
+		cout << "Su opcion: ";
+		cin >> opcion;
+	}while(opcion > 0 && opcion < 8);
+
+	return opcion;
 }
